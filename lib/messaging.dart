3f5e462c0b4a97aaf970851a150423b987fbb516
Mathai Mathew <mathewp8616@gmail.com>
2021-12-24 19:12:58 +0530

    Small Changes to 2nd pages.

--- lib/messaging.dart
+++ lib/messaging.dart
@@ -12,11 +12,16 @@ class Messaging extends StatelessWidget {
     return Scaffold(
         backgroundColor: Colors.black,
         appBar: AppBar(
-        title: Text("Profile Page"),
-    centerTitle: true,
-    backgroundColor: Colors.black,
-    elevation: 0.0,
-    ),
-    );
+          title: Text("Profile Page"),
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.video_call), onPressed: () {}),
+            IconButton(icon: Icon(Icons.call), onPressed: () {}),
+          ],
+          centerTitle: true,
+          backgroundColor: Colors.black,
+          elevation: 0.0,
+        ),
+        body: Center(),
+        );
   }
 }
